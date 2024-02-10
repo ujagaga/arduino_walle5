490a07e16a7e233a3e0d371505f44d0e6d1fd141
Rada Berar <ujagaga@gmail.com>
2024-02-10 13:51:04 +0100

    fixing range

--- Hardware/ESP-eye/tof_range.cpp
+++ Hardware/ESP-eye/tof_range.cpp
@@ -7,13 +7,16 @@ DFRobot_VL6180X VL6180X(TOF_1_IIC_ADDRESS, &I2C);
 
 
 bool TOF_init(){
-  I2C.begin(I2C_SDA, I2C_SCL, 200000);
-  return VL6180X.begin();
+  I2C.begin(I2C_SDA, I2C_SCL, 200000);  
+  bool result = VL6180X.begin();
+  VL6180X.rangeSetInterMeasurementPeriod(30);
+  return result;
 }
 
 uint8_t TOF_get_range(){
   uint8_t range = VL6180X.rangePollMeasurement();
   uint8_t status = VL6180X.getRangeResult();
+  
   if(status == VL6180X_NO_ERR){
     return range;
   }
